ff2f05e45d965aaeefde61b2fca4508e0eb4c650
nashon satat <nahashon.satat@gmail.com>
2019-05-02 20:03:00 +0300

    I am implement the function guess

--- Guesser.hpp
+++ Guesser.hpp
@@ -10,13 +10,22 @@ class Guesser
 
 private:
   string guessOption;
-  uint bull;
-  uint pgia;
+ // uint bull;
+  //uint pgia;
 
 public:
-  uint length;
+uint length;
+   
+  uint bull;
+  uint pgia;
+  int numberOfAttempts=0;
+  string back="";
+  uint backbull=0;
+  int count=0;
+  string guesss="";
   virtual string guess() = 0;
   virtual void startNewGame(uint length) { this->length = length; };
   virtual void learn(string replay);
+  
 };
 } // namespace bullpgia
--- SmartGuesser.cpp
+++ SmartGuesser.cpp
@@ -2,7 +2,63 @@
 #include <iostream>
 using std::string ; 
 
-std::string bullpgia::SmartGuesser::guess(){return "";}
+std::string bullpgia::SmartGuesser::guess(){
+    if (this->length==1)this->back="0";
+    if (this->length==2)this->back="00";
+    if (this->length==3)this->back="000";
+    if (this->length==4)this->back="0000";
+    if (this->length==5)this->back="00000";
+    if (this->length==6)this->back="000000";
+    if (this->length==7)this->back="0000000";
+    
+    
+    if (this->numberOfAttempts==0){
+        
+        this->numberOfAttempts++;
+       this-> guesss=this->back;
+        return this->back;
+    }
+    else if (this->numberOfAttempts==1){
+       this-> backbull=this->bull;
+        this->numberOfAttempts++;
+       this-> guesss[0]=this->guesss[0]+1;
+        return this->guesss;
+    }
+    else{
+        if (this->backbull>this->bull){
+           this-> numberOfAttempts++;
+        this-> guesss[count]=this->guesss[count]-1;
+         
+         this->count++;
+        this-> guesss[count]++;
+         
+         return this-> guesss;
+        }
+        else if (this->backbull==this->bull){
+            this->numberOfAttempts++;
+            this->back=this->guesss;
+            this->guesss[count]++;
+            return this->guesss;
+            
+        }
+        else
+        {
+            this->numberOfAttempts++;
+            this->backbull=this->bull;
+           
+           this-> back=this->guesss;
+            this->count++;
+           this-> guesss[count]++;
+            return this->guesss;
+        }
+        
+
+    }
+    
+    
+    
+    
+    }
 void bullpgia::Guesser::learn(std::string reaply)
 {
     bull = std::stoi(reaply.substr(0,reaply.find(',')));
